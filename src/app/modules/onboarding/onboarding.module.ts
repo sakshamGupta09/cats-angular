ead458ae57997c97b52f09ecff9ed95f8a11ab1c
Saksham Gupta <sakgupta8@publicisgroupe.net>
2021-11-20 10:22:20 +0530

    login ui ready

--- src/app/modules/onboarding/onboarding.module.ts
+++ src/app/modules/onboarding/onboarding.module.ts
@@ -2,20 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OnboardingRoutingModule } from './onboarding-routing.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
-
 @NgModule({
-  declarations: [
-    LoginComponent,
-    SignupComponent,
-    ForgotPasswordComponent
-  ],
+  declarations: [LoginComponent, SignupComponent, ForgotPasswordComponent],
   imports: [
     CommonModule,
-    OnboardingRoutingModule
-  ]
+    OnboardingRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
 })
-export class OnboardingModule { }
+export class OnboardingModule {}
